f5290b4aad06f54433580aff8f04cc59b0978931
0xAquaWolf <0xaquawolf@protonmail.com>
2024-08-28 14:03:29 -0400

    added favicon to website

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-// import {
-//   Accordion,
-//   AccordionContent,
-//   AccordionItem,
-//   AccordionTrigger,
-// } from "@/components/ui/accordion"
-// import { WavyBackgroundDemo } from "@/components/ui/WavyBackgroundDemo"
-
 import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
