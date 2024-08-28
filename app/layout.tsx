4d0a6f91f898c661cc86770cd9a7d7689af6a914
0xAquaWolf <0xaquawolf@protonmail.com>
2024-08-28 14:09:19 -0400

    updated description

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,8 @@ import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "0xAquaWolf Portfolio & Blog",
-  description: "",
+  description:
+    "AquaWolf is a senior full-stack Web3 software engineer and indie hacker with a proven track record in building impactful, consumer-facing products that have generated over $28M in Web3 projects and $30K MRR in e-commerce. Specializing in rapid prototyping, full-stack development, UI/UX design, and emerging technologies like AI and blockchain, AquaWolf is passionately building innovative solutions and upskilling in Web3, AI, and Machine Learning while releasing quality apps to the public.",
   icons: {
     icon: [
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
@@ -36,13 +37,15 @@ export const metadata: Metadata = {
   openGraph: {
     type: "website",
     siteName: "Your Site Name",
-    title: "Your Site Title",
-    description: "Your site description",
+    title: "0xAquaWolf Portfolio & Blog",
+    description:
+      "AquaWolf is a senior full-stack Web3 software engineer and indie hacker with a proven track record in building impactful, consumer-facing products that have generated over $28M in Web3 projects and $30K MRR in e-commerce. Specializing in rapid prototyping, full-stack development, UI/UX design, and emerging technologies like AI and blockchain, AquaWolf is passionately building innovative solutions and upskilling in Web3, AI, and Machine Learning while releasing quality apps to the public.",
   },
   twitter: {
     card: "summary",
-    title: "Your Site Title",
-    description: "Your site description",
+    title: "0xAquaWolf Portfolio & Blog",
+    description:
+      "AquaWolf is a senior full-stack Web3 software engineer and indie hacker with a proven track record in building impactful, consumer-facing products that have generated over $28M in Web3 projects and $30K MRR in e-commerce. Specializing in rapid prototyping, full-stack development, UI/UX design, and emerging technologies like AI and blockchain, AquaWolf is passionately building innovative solutions and upskilling in Web3, AI, and Machine Learning while releasing quality apps to the public.",
   },
 };
 
