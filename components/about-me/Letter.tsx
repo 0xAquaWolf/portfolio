b4e20ad8fa8976b59c5946065716c92c9665498e
0xAquaWolf <0xaquawolf@protonmail.com>
2024-09-04 14:33:28 -0400

    fixed errors in text

--- components/about-me/Letter.tsx
+++ components/about-me/Letter.tsx
@@ -10,29 +10,31 @@ export default function Page() {
       <div className="bg-letter-top shadow-letter-top relative -top-3 z-10 -rotate-1 rounded-lg lg:rotate-2 lg:rounded-xl">
         <article className="space-y-4 p-4 text-base text-white/80 lg:space-y-5 lg:p-5 lg:px-24 lg:py-14 lg:text-2xl">
           <p>Hey everyone—</p>
-          <p>I'm AquaWolf, and I've been on quite a journey.</p>
+          <p>I&apos;m AquaWolf, and I&apos;ve been on quite a journey.</p>
           <p>
-            I've been coding since 2016, and let me tell you, it's been a wild
-            ride. Coding gets a bad rap sometimes, but it shouldn't. To me,
-            coding has always been a treasure.
+            I&apos;ve been coding since 2016, and let me tell you, it&apos;s
+            been a wild ride. Coding gets a bad rap sometimes, but it
+            shouldn&apos;t. To me, coding has always been a treasure.
           </p>
           <p>
-            There's nothing quite like the feeling of creating something from
-            nothing, or solving a complex problem that's been nagging at you for
-            days. That rush of satisfaction when your code finally runs
-            smoothly—that's how coding used to feel all the time for me.
+            There&apos;s nothing quite like the feeling of creating something
+            from nothing, or solving a complex problem that&apos;s been nagging
+            at you for days. That rush of satisfaction when your code finally
+            runs smoothly—that&apos;s how coding used to feel all the time for
+            me.
           </p>
           <p>But things changed.</p>
           <p>
             In 2021, I got swept up in the crypto bull run. I helped launch
             various NFT projects, including DAW (Desperate Ape Wives) on ETH and
-            TheLostKingdom on FTM. We're talking millions in sales and thousands
-            in trading volume. It was exhilarating, but it came at a cost.
+            TheLostKingdom on FTM. We&apos;re talking millions in sales and
+            thousands in trading volume. It was exhilarating, but it came at a
+            cost.
           </p>
           <p>
             I lost control over my well-being. An avalanche of work and stress
             buried me. I was coding for 12 hours a day, smoking weed just to
-            cope with the pain. And still, it wasn't enough.
+            cope with the pain. And still, it wasn&apos;t enough.
           </p>
           <p>
             The industry just let it happen. No one seemed to care about my
@@ -45,8 +47,8 @@ export default function Page() {
           </p>
           <p>
             But you know what? Even after all that, coding still remains a
-            wonder to me. It's powerful. It's creative. It connects people
-            across the globe and helps millions earn a living.
+            wonder to me. It&apos;s powerful. It&apos;s creative. It connects
+            people across the globe and helps millions earn a living.
           </p>
           <p>
             The magic was still there, just buried under a mess of burnout and
@@ -72,15 +74,17 @@ export default function Page() {
             found the peace I was looking for.
           </p>
           <p>
-            Now, I'm no longer afraid to share my story. I'm a Senior Full-Stack
-            developer, yes, but I'm also a shaman, a time traveler, a hacker, a
-            tinker, a delusional optimist, a truth seeker, a data broker, an AI
-            researcher, and a polymath.
+            Now, I&apos;m no longer afraid to share my story. I&apos;m a Senior
+            Full-Stack developer, yes, but I&apos;m also a shaman, a time
+            traveler, a hacker, a tinker, a delusional optimist, a truth seeker,
+            a data broker, an AI researcher, and a polymath.
           </p>
-          <p>To everyone out there feeling lost or burnt out: there's hope.</p>
+          <p>
+            To everyone out there feeling lost or burnt out: there&apos;s hope.
+          </p>
           <p>In the chaos there is always balance to find</p>
           <p>
-            This is my journey, and I'm still allowing the expansion to
+            This is my journey, and I&apos;m still allowing the expansion to
             continue. May it inspire you to find your own balance in this wild
             world of technology and beyond.
           </p>
