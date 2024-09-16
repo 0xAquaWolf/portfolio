6b6cba8d3a594a50a78599f03eb44610bae5c8d7
0xAquaWolf <0xaquawolf@protonmail.com>
2024-09-15 20:56:14 -0400

    update syntax highlighting to include rehype highlight metadata

--- lib/markdownToHTML.ts
+++ lib/markdownToHTML.ts
@@ -4,13 +4,11 @@ import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
 import rehypePrettyCode from 'rehype-pretty-code';
 import { transformerCopyButton } from '@rehype-pretty/transformers';
-import rehypeRaw from 'rehype-raw';
 
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse)
     .use(remarkRehype, { allowDangerousHtml: true })
-    .use(rehypeRaw)
     .use(rehypePrettyCode, {
       theme: 'catppuccin-macchiato',
       transformers: [
@@ -23,5 +21,5 @@ export default async function markdownToHtml(markdown: string) {
     .use(rehypeStringify as any)
     .process(markdown);
 
-  return result.toString();
+  return String(result);
 }
