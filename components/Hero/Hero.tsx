e7df14c649e131e13cdb8797c92f3de5e613f6b9
rushil <rushiljalal@gmail.com>
2024-11-02 18:24:30 +0530

    add padding to hero section

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,11 +1,11 @@
-import SVGGradientBg from "./SVGGradientBg";
-import Menu from "./Menu/Menu";
-import HeroCTA from "./HeroCTA";
+import SVGGradientBg from './SVGGradientBg';
+import Menu from './Menu/Menu';
+import HeroCTA from './HeroCTA';
 
 export default function Hero() {
   return (
-    <div className="relative">
-        <HeroCTA />
+    <div className="relative pl-10">
+      <HeroCTA />
     </div>
   );
 }
