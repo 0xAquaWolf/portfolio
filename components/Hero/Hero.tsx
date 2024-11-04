b8ac9f37d65b963ff1340b8b73ea883dd019cd89
rushil <rushiljalal@gmail.com>
2024-11-04 09:59:52 +0530

    fix

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -4,7 +4,7 @@ import HeroCTA from './HeroCTA';
 
 export default function Hero() {
   return (
-    <div className="relative pl-10">
+    <div className="relative px-10">
       <HeroCTA />
     </div>
   );
