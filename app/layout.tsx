856d71b90b1c5e1886ad089d08bef2f88c096254
0xAquaWolf <aquawolf369@gmail.com>
2024-11-23 14:39:04 -0400

    fixed overflow issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -97,7 +97,7 @@ export default function RootLayout({
         )}
       >
         <Providers>
-          <div className="mx-auto max-w-[1440px] overflow-y-hidden px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-[1440px] bg-transparent px-4 sm:px-6 lg:px-8">
             <SVGGradientBg />
             <Menu />
             {children}
