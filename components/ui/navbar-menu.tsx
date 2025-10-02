63981b6bda86a16d8ba64839578f26b0e59b509b
0xAquaWolf <aquawolf369@gmail.com>
2025-10-02 10:49:15 -0500

    updated eslint and image tag

--- components/ui/navbar-menu.tsx
+++ components/ui/navbar-menu.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { motion } from "motion/react";
-
+import Image from 'next/image';
 
 
 const transition = {
@@ -101,7 +101,7 @@ export const ProductItem = ({
 }) => {
   return (
     <a href={href} className="flex space-x-2">
-      <img
+      <Image
         src={src}
         width={140}
         height={70}
